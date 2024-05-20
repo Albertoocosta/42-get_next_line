47d665548de99fdc453a83001d755770c06ce845
Carlos Alberto Da Fonseca Costa Neto <cda-fons@c1r14s5.42porto.com>
2024-05-20 21:08:37 +0100

    FEAT: saving process

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:30:07 by cda-fons          #+#    #+#             */
-/*   Updated: 2024/05/19 15:31:27 by cda-fons         ###   ########.fr       */
+/*   Updated: 2024/05/20 21:07:50 by cda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 char	*get_next_line(int fd)
 {
+	static char	*cluster;
+	char		*reader;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	cluster = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	reader = (read(fd, cluster, BUFFER_SIZE));
+	while (cluster[i] != '\n' && cluster[i])
+		i++;
+	if (cluster[i] == '\n')
+	{
+		while (j < i)
+			write(1, cluster[j], 1);
+	}
 	
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: cda-fons <cda-fons@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:31:38 by cda-fons          #+#    #+#             */
-/*   Updated: 2024/05/19 15:35:55 by cda-fons         ###   ########.fr       */
+/*   Updated: 2024/05/20 20:12:10 by cda-fons         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 42
+# endif
 # include <unistd.h>
 # include <stdlib.h>
 
