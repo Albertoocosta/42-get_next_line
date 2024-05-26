3c8a503783e070e805e3c984f171f4b764dc1190
Alberto Costa <albertoocostapt@gmail.com>
2024-05-26 01:06:07 +0100

    FEAT: Add some funtions, project almost complete

--- get_fts.c
+++ get_fts.c
@@ -12,3 +12,53 @@
 
 #include "get_next_line.h"
 
+size_t	ftstrlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != 0)
+		i++;
+	return (i);
+}
+
+char	*ftstrchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != (char)c)
+	{
+		if (!str[i])
+			return (0);
+		i++;
+	}
+	return ((char *)&str[i]);
+}
+
+char	*ftstrjoin(char const *s1, char const *s2)
+{
+	char	*cat;
+	size_t	i;
+	size_t	h;
+
+	cat = malloc(sizeof (char) * ((ft_strlen((char *)s1) + ft_strlen((char *)s2)
+					) + 1));
+	if (cat == 0)
+		return (NULL);
+	i = 0;
+	while (s1[i] != 0)
+	{
+		cat[i] = s1[i];
+		i++;
+	}
+	h = 0;
+	while (s2[h] != 0)
+	{
+		cat[i] = s2[h];
+		h++;
+		i++;
+	}
+	cat[i] = '\0';
+	return (cat);
+}
--- get_next_line.c
+++ get_next_line.c
@@ -12,17 +12,90 @@
 
 #include "get_next_line.h"
 
+char	*newcluster(char *cluster)
+{
+	char	*word;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (cluster[i] && cluster[i] != '\n')
+		i++;
+	if (!cluster[i])
+		return (free(cluster), NULL);
+	word = (char *)malloc(sizeof(char) * (ftstrlen(cluster) - i + 1));
+	if (!word)
+		return (NULL);
+	i++;
+	j = 0;
+	while (cluster[i])
+	{
+		word[j] = cluster[i];	
+		j++;
+		i++;
+	}
+	word[j] = '\0';
+	free(cluster);
+	return (word);
+}
+
+char	*get_line(char *cluster)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	while (cluster[i] && cluster[i] != '\n')
+		i++;
+	word = (char *)malloc(sizeof(char) * (i + 2));
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (cluster[i] && cluster[i] != '\n')
+	{
+		word[i] = cluster[i];
+	}
+	if (cluster[i] == '\n')
+	{
+		word[i] = cluster[i];
+		i++;
+	}
+	word[i] = '\0';
+	return(word);
+}
+
+char	*readfile(int fd, char *cluster)
+{
+	char	*buffer;
+	int	counter;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	counter = 1;
+	while (!ftstrchr(cluster, '\n') && counter != 0)
+	{
+		counter = read(fd, buffer, BUFFER_SIZE);
+		if (counter == -1)
+			return (free(buffer), NULL);
+		buffer[counter] = '\0';
+		cluster = ftstrjoin(cluster, buffer);
+	}
+	free(buffer);
+	return (cluster);
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*cluster;
-	int	reader;
+	static char		*cluster;
+	char			reader;
 
-	cluster = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	cluster = readfile(fd, cluster);
 	if (!cluster)
 		return (0);
-	reader = (read(fd, cluster, BUFFER_SIZE));
-	if (reader <= 0)
-		free(cluster);
-		return (0);	
-	return (cluster);
+	reader = get_line(cluster);
+	cluster = newcluster(cluster);
+	return (reader);
 }
